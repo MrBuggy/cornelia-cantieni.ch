741db442698e2564cdb9370715f66a8bc30e1147
Srdan Turudic <srdan.turudic@namics.com>
2018-11-14 16:26:20 +0100

    CANWEB-001: linting

--- cornelia-cantieni/src/app/app.component.ts
+++ cornelia-cantieni/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'cornelia-cantieni';
+    title = "cornelia-cantieni";
 }
