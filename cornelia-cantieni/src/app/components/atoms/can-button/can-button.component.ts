e36b26fb12cab9cd4edb84951811577d4c904677
Srdan Turudic <srdan.turudic@namics.com>
2018-11-26 15:10:58 +0100

    CANWEB-008: Add disabled button state to view

--- cornelia-cantieni/src/app/components/atoms/can-button/can-button.component.ts
+++ cornelia-cantieni/src/app/components/atoms/can-button/can-button.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class CanButtonComponent implements OnInit {
     @Input() modifier: string;
+    @Input() state: string;
     @Input() text: string = "Default button";
 
     className: string = "can-button";
