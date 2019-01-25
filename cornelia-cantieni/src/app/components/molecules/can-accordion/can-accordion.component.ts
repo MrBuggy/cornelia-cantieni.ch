63581fc2d21e7874bb72711e7a143cc9594e2e84
Srdan Turudic <srdan.turudic@namics.com>
2019-01-25 18:35:45 +0100

    CANWEB-015: accordion component

--- cornelia-cantieni/src/app/components/molecules/can-accordion/can-accordion.component.ts
+++ cornelia-cantieni/src/app/components/molecules/can-accordion/can-accordion.component.ts
@@ -6,27 +6,54 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./can-accordion.component.scss']
 })
 export class CanAccordionComponent implements OnInit {
+    activeIndex: number = 0;
     accordionList: Array<object> = [
         {
             title: "Coaching",
-            text: "Lorem Ipsum"
+            text: "Begleitung in Lebensphasen. Beratung, welche prozessorientierte Lösungen für Fragen im Beruf oder des Alltaglebens schafft."
         },
         {
             title: "Supervision",
-            text: "Lorem Ipsum"
+            text: "Übersicht durch Aussensicht. Die Motivation und die Zufriedenheit Ihrer Mitarbeitenden resultieren aus der professionellen Reflexion von konkreten Lebens- und Erfahrungssituationen. Ziele sind die Standortbestimmung, die Prozessbegleitung, die Chancenerkennung und die Problemlösung."
         },
         {
             title: "Beratung",
-            text: "Lorem Ipsum"
+            text: "Hilfe zur Selbsthilfe. Die Entwicklung Ihrer Organisation ist die Sicherstellung eines dynamischen Prozesses, um die Abläufe und die Zusammenarbeit wirksam zu gestalten. Kompetenz, Vertrauen und Sympathie – damit stärken Sie die Kommunikations-, die Kooperations- und die Organisationsfähigkeit von Teams."
         },
         {
             title: "Bildung",
-            text: "Lorem Ipsum"
+            text: "Bewegung im Kopf, im Geist, in jeder Zelle des Menschen. Ihre Fort- und Weiterbildung ist die Antwort auf die konstante Herausforderung, neuem Wissen und Veränderungen im Beruf zu begegnen. Ziele sind die Bereicherung, die Entwicklung und die Festigung Ihrer Fähigkeiten."
         }
-    ]
+    ];
+    collapsed: boolean = false;
 
     constructor() { }
 
     ngOnInit() {
+    }
+
+    toggleActiveItem(i: number): void {
+        this.collapsed = true;
+        this.activeIndex = i;
+    }
+
+    resetAccordion(i: number, event: Event) {
+        if (i === this.activeIndex && this.collapsed) {
+            event.stopPropagation();
+            this.collapsed = false;
+            const items = document.querySelectorAll(".can-accordion__item");
+            [].forEach.call(items, (item: HTMLElement) => {
+                item.classList.remove("state-can-accordion__item--open");
+                item.classList.remove("state-can-accordion__item--collapsed");
+            });
+        }
+    }
+
+    isOpen(i: number) {
+        return this.activeIndex === i && this.collapsed;
+    }
+
+    isCollapsed(i: number) {
+        return this.activeIndex != i && this.collapsed;
     }
 }
